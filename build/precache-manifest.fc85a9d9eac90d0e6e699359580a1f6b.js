353ed0351c0ebf04c9e8a437b64c00fb3faa3294
xavierxavier2364@yahoo.com <xavierxavier2364@yahoo.com>
2020-12-03 02:48:01 +0800

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.fc85a9d9eac90d0e6e699359580a1f6b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26499242da0793f4112e3f8de9034fea",
+    "url": "/amex23/amex23.github.io.git/index.html"
+  },
+  {
+    "revision": "efca7d7d64f34613a721",
+    "url": "/amex23/amex23.github.io.git/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "941cb9c5fdfae28ce536",
+    "url": "/amex23/amex23.github.io.git/static/css/main.ae8edba4.chunk.css"
+  },
+  {
+    "revision": "efca7d7d64f34613a721",
+    "url": "/amex23/amex23.github.io.git/static/js/2.3eadef52.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/amex23/amex23.github.io.git/static/js/2.3eadef52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "941cb9c5fdfae28ce536",
+    "url": "/amex23/amex23.github.io.git/static/js/main.79aa1a05.chunk.js"
+  },
+  {
+    "revision": "6f41fce98a535eb46d5e",
+    "url": "/amex23/amex23.github.io.git/static/js/runtime-main.8c7e88bf.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/amex23/amex23.github.io.git/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/amex23/amex23.github.io.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/amex23/amex23.github.io.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/amex23/amex23.github.io.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/amex23/amex23.github.io.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/amex23/amex23.github.io.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/amex23/amex23.github.io.git/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/amex23/amex23.github.io.git/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/amex23/amex23.github.io.git/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/amex23/amex23.github.io.git/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/amex23/amex23.github.io.git/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/amex23/amex23.github.io.git/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/amex23/amex23.github.io.git/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/amex23/amex23.github.io.git/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/amex23/amex23.github.io.git/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/amex23/amex23.github.io.git/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/amex23/amex23.github.io.git/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/amex23/amex23.github.io.git/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/amex23/amex23.github.io.git/static/media/myImage.bd80794f.png"
+  }
+]);
